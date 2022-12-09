01a06cd7c05d1212cd30814f94294ae6eb6607b7
Rusy13 <rusugarov@gmail.com>
2022-12-09 18:05:51 +0300

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :usercustomers
+  devise_for :userexecutors
+  root 'open#page1', as: 'home'
+  get 'open/page1'
+  get 'open/page2'
 end
---
+++ test/models/userexecutor_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class UserexecutorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
